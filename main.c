0854c65ac906b9cf89ef5dd4052d2709daa85031
kocierik <kocierik@gmail.com>
2022-02-20 17:13:25 +0100

    feat: add hostname

--- main.c
+++ main.c
@@ -5,9 +5,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#define MAXLEN 50
 struct machine{
+  int len;
   char *name;
   struct machine *next;
+  struct machine *prev;
 };
 
 int addNodeBottom(char *name, struct machine *head){
@@ -16,13 +19,50 @@ int addNodeBottom(char *name, struct machine *head){
     strcpy(newNode->name,name);
     newNode->next = NULL; 
 
-    if(head->next == NULL) head->next = newNode;
+    if(head->next == NULL){
+       head->next = newNode;
+       head->prev = NULL;
+    }
     else{
       struct machine *current = head;
       while (current->next != NULL) current = current->next;
       current->next = newNode;
+      newNode->prev = current;
     }
     return 0;
+}
+
+
+int listLen(struct machine *list){
+  int i = 0;
+  while (list != NULL){
+    i++;
+    list = list->next;
+  }
+  return i;
+}
+
+struct machine* parseFile(){
+  struct passwd *pw = getpwuid(getuid());
+  char *homedir = pw->pw_dir;
+  strcat(homedir,"/.ssh/known_hosts.old");
+  FILE *file = fopen(homedir,"r"); 
+
+  ssize_t read;
+  char *line = NULL;
+  size_t len = 0;
+  char *text = NULL;  
+  struct machine* headList = (struct machine*)malloc(sizeof(struct machine));
+  headList->next = NULL;
+
+  while ((read = getline(&line, &len, file)) != -1) {
+    text = strtok(line,",");
+    strtok(line," ");
+    // printf("%s\n",text);
+    addNodeBottom(text,headList);
+  }
+  fclose(file);
+  return headList;
 }
 
 
@@ -30,30 +70,30 @@ int addNodeBottom(char *name, struct machine *head){
 
 int main() {
   WINDOW *w;
-  
-  struct passwd *pw = getpwuid(getuid());
-  char *homedir = pw->pw_dir;
-  char *line = NULL;
-  // bool checkSpace = false;
-  strcat(homedir,"/.ssh/known_hosts.old");
-  FILE *file = fopen(homedir,"r"); 
+  int lenList = 0;
+  struct machine *temp = parseFile();
+  struct machine *headList = temp;
+  headList = headList->next;
+  free(temp);
+  headList->len = listLen(headList);
+  struct machine *movePt = headList;
 
 
-  
-  char list[5][7] = { "One", "Two", "Three", "Four", "Five" };
+
   char item[8];
   int ch, i = 0;
   initscr(); 
-  w = newwin(10, 30, 1, 1); 
+  w = newwin(listLen(headList)+2, 45, 1, 1); 
   box(w, 0, 0); 
   
-  for( i=0; i<5; i++ ) {
+  for( i=0; i<9; i++ ) {
     if( i == 0 ) 
     wattron( w, A_STANDOUT ); // highlights the first item.
     else
       wattroff( w, A_STANDOUT );
-    sprintf(item, "%-7s",  list[i]);
-    mvwprintw( w, i+1, 2, "%s", item );
+    // sprintf(item, "%s", headList->name);
+    mvwprintw( w, i+1, 2, "%s", movePt->name );
+    movePt = movePt->next;
     }
   
   wrefresh(w);
@@ -61,50 +101,30 @@ int main() {
   noecho(); 
   keypad(w, TRUE); 
   curs_set( 0 ); 
-
+  movePt = headList;
   while(( ch = wgetch(w)) != 'q'){ 
     // right pad with spaces to make the items appear with even width.
-    sprintf(item, "%-7s",  list[i]); 
-    mvwprintw( w, i+1, 2, "%s", item ); 
+    mvwprintw( w, i+1, 2, "%s", movePt->name ); 
 
     switch( ch ) {
       case KEY_UP:
-        i--;
-        i = ( i<0 ) ? 4 : i;
-        break;
+        if(i>0){
+            i--;
+            movePt = movePt->prev;
+            break;
+          }
       case KEY_DOWN:
-        i++;
-        i = ( i>4 ) ? 0 : i;
-        break;
+        if(i<8){
+          i++;
+          movePt = movePt->next;
+          break;
+        }
     }
     wattron( w, A_STANDOUT );
-    sprintf(item, "%-7s",  list[i]);
-    mvwprintw( w, i+1, 2, "%s", item);
+    mvwprintw( w, i+1, 2, "%s", movePt->name);
     wattroff( w, A_STANDOUT );
   }
   delwin(w);
   endwin();
-
-  ssize_t read;
-  size_t len = 0;
-  char *text = NULL;
-  char *text2 = NULL;
-  
-  struct machine* headList = (struct machine*)malloc(sizeof(struct machine));
-  headList->next = NULL;
-
-  
-
-  while ((read = getline(&line, &len, file)) != -1) {
-    text = strtok(line,",");
-    text2 = strtok(line," ");
-    // printf("%s\n",text);
-    addNodeBottom(text,headList);
-  }
-  while (headList != NULL){
-    printf("%s\n",headList->name);
-    headList = headList->next;
-  }
-
-    fclose(file);
+  return 0;
 }
